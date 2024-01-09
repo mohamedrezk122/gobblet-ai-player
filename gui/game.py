27c7c4b4d167e379094d793a6448941a3d6f3f5c
Mohamed Rezk <mohrizq895@gmail.com>
2024-01-09 11:28:09 +0200

    integrate with game logic

--- gui/game.py
+++ gui/game.py
@@ -1,9 +1,13 @@
+import time 
 import pygame 
-import pygame_menu as pgm 
+import pygame_menu as pgm
 
 from .board import Board
 from .drag_handler import Drag_Handler
 from .constants import * 
+
+clock = pygame.time.Clock()
+
 
 class Game:
 
@@ -12,11 +16,14 @@ class Game:
         self.board = Board(game_window)
         self.drag_handler = Drag_Handler(self)
 
-        self.current_player = "w"
+        self.current_player = "w" # white starts by default 
         self.game_mode = None 
         self.difficulty = None 
         self.intialize_labels()
         self.turn_label = self.game_window.get_widget('turn')
+
+        self.winner = None 
+        self.end_of_game = False
 
     def reset(self):
         self.game_window.full_reset()
@@ -24,6 +31,7 @@ class Game:
         self.board = Board(self.game_window)
         self.current_player = 'w'
         self.intialize_labels()
+        self.end_of_game = False
 
     def set_difficulty(self,selected, value):
         print(selected ," ", value)
@@ -37,10 +45,34 @@ class Game:
         self.current_player = "b" if self.current_player == "w" else "w"
         self.turn_label.set_title(PLAYER[self.current_player][0])
         self.turn_label.update_font({"color" : PLAYER[self.current_player][2]})
+        self.check_game_state()
 
     def check_game_state(self):
-        pass  
+        self.check_row_win()
+        self.check_col_win()
+        self.check_diagonal_win()
+        self.check_diagonal_win(right=True)
 
+        if self.winner:
+            self.raise_end_of_game_flag()
+
+    def raise_end_of_game_flag(self):
+        self.end_of_game = True
+
+        # remove whose turn 
+        self.turn_label.set_title("-")
+        self.turn_label.update_font({"color" : "#ff0000"})
+        
+        # declare winner
+        winner = PLAYER[self.winner][0]
+        label = self.game_window.add.label(f"{winner} WON!",font_size=80, font_color="#ff0000")
+        label.set_float(origin_position=True)
+        label.translate(WIDTH//2-label.get_width()//2, HEIGHT-label.get_height() - 60)
+        
+        # self.reset()
+
+    def check_draw_state(self):
+        pass
     def move_piece_by_human(self):
         pass
 
@@ -64,3 +96,56 @@ class Game:
         turn_label = self.game_window.add.label("White", label_id = "turn",font_size=50, font_color="#ffffff")
         turn_label.set_float(origin_position=True)
         turn_label.translate(WIDTH//2 - 130 , 30)
+
+
+    def check_row_win(self):
+        for row in range(BOARD_ROWS):
+            top_colors_row = []
+            for column in range(BOARD_COLS):
+                top =  self.board.get_top_piece(row, column)
+                if not top :
+                    break
+                top_colors_row.append(top.player)
+
+            if not top_colors_row: 
+                continue
+            if self.count_colors(top_colors_row, "Row Win: "):
+                return
+
+    def check_col_win(self):
+        for column  in range(BOARD_COLS):
+            top_colors_col = []
+            for row in range(BOARD_ROWS):
+                top =  self.board.get_top_piece(row, column)
+                if not top :
+                    break
+                top_colors_col.append(top.player)
+
+            if not top_colors_col: 
+                continue
+
+            if self.count_colors(top_colors_col, "Col Win: "): 
+                return
+
+    def check_diagonal_win(self, right=False):
+        # default is left diagonal checking 
+        top_colors_diagonal = []
+        for i in range(BOARD_COLS):
+            y = 3 - i if right else i 
+            top =  self.board.get_top_piece(i, y)
+            if not top :
+                break
+            top_colors_diagonal.append(top.player)
+
+            msg = f"{'Right' if right else 'Left'} Diagonal Win: "
+            if self.count_colors(top_colors_diagonal, msg):
+                return
+
+    def count_colors(self, colors, msg=""):
+        # print(colors)
+        if colors.count(colors[0]) == 4:
+            self.winner = colors[0]
+            if msg :
+                print(msg + self.winner)
+            return True  # there is a winner 
+        return False     # game continues 
