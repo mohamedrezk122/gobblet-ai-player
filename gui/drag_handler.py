f43d2453814000711d9c7d4d1723d9b3e8cf62c3
Mohamed Rezk <mohrizq895@gmail.com>
2024-01-21 12:02:44 +0200

    add human vs ai mode

--- gui/drag_handler.py
+++ gui/drag_handler.py
@@ -1,10 +1,18 @@
-import pygame 
-import pygame_menu as pgm 
+import pygame
+import pygame_menu as pgm
 
 from .constants import *
 
+import numpy as np 
 pygame.mixer.init()
 move_sound = pygame.mixer.Sound("./gui/assets/move.mp3")
+
+
+def convert_from_row_col_to_abs_pos(row, col, piece_width, piece_height):
+    dx,dy = -piece_width // 2, -piece_height // 2
+    x = BOARD_POS_X + (col + 0.5) * (CELL_WIDTH) + dx
+    y = BOARD_POS_Y + (row + 0.5) * (CELL_HEIGHT) + dy
+    return x, y
 
 
 class Drag_Handler:
@@ -13,6 +21,9 @@ class Drag_Handler:
         self.drag = False
         self.past_pos = None
         self.game = game
+
+        # a list constaining the info of the move from which cell to which, etc.
+        self.augmented_move = None
 
     def move_piece_with_mouse(self, pieces, event):
         piece = pieces[self.active]
@@ -25,17 +36,17 @@ class Drag_Handler:
         active_pieces = []
         for idx, piece in enumerate(pieces):
             if piece.collidepoint(event.pos):
-                if piece.player != current_player :
-                    continue 
+                if piece.player != current_player:
+                    continue
                 active_pieces.append(idx)
-        
+
         max_so_far = -1
-        # choose the one on top 
-        for piece_idx in active_pieces :
+        # choose the one on top
+        for piece_idx in active_pieces:
             piece = pieces[piece_idx]
-            if max_so_far < SIZE[piece.size][-1] :
-                max_so_far = SIZE[piece.size][-1] 
-                active = piece_idx   
+            if max_so_far < SIZE[piece.size][-1]:
+                max_so_far = SIZE[piece.size][-1]
+                active = piece_idx
         if not self.drag and active is not None:
             self.past_pos = pieces[active].get_position()
         return active
@@ -44,7 +55,7 @@ class Drag_Handler:
         if self.drag and self.active is not None:
             col, row = self.get_tile_under_mouse(event.pos)
             if col is not None and row is not None:
-                self.check_rules(board,pieces,row , col, event)
+                self.check_rules(board, pieces, row, col, event)
             else:
                 self.return_piece(pieces, event)
         self.active = None
@@ -59,11 +70,14 @@ class Drag_Handler:
         else:
             return None, None
 
-    def update(self,events):
-        board  = self.game.get_board().board
+    def update(self, events, is_ai_player=False, *args):
+        board = self.game.get_board().board
         pieces = self.game.get_board().get_pieces()
 
-        if self.game.end_of_game :
+        if is_ai_player:
+            self.move_piece_by_ai(board, pieces, *args)
+
+        if self.game.end_of_game:
             return
         for event in events:
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -74,6 +88,9 @@ class Drag_Handler:
                 self.move_piece_with_mouse(pieces, event)
 
     def interpolate_path(self, point1, point2, num=100):
+        if (point1[0] - point2[0]) == 0 :
+            dy = (point2[1] - point1[1])/num
+            return [[point1[0],point1[1]+i*dy] for i in range(num)]+list(point2)
         slope = (point1[1] - point2[1]) / (point1[0] - point2[0])
         points = [point1]
         dx = (point1[0] - point2[0]) // num
@@ -86,41 +103,88 @@ class Drag_Handler:
 
     def move_automatically(self, points, piece):
         for pt in points:
-            piece.set_pos(int(pt[0]), int(pt[1]))
+            try:
+                piece.set_pos(int(pt[0]), int(pt[1]))
+            except:
+                print(pt)
+    def move_piece_by_ai(self, board, pieces, move):
+        piece = None
+        print("-"*20)
+        print(move)
+        print("-"*20)
+        if move.from_outside == "Y":
+            for pece in pieces:
+                if all(
+                    [
+                        pece.player == move.piece_color,
+                        SIZE[pece.size][-1]+1 == move.piece_size,
+                        pece.row == None,
+                        pece.col == None,
+                        pece.aside_stack-1 == move.stack,
+                    ]
+                ):
+                    print("here")
+                    piece = pece
+                    break
+            if piece:
+                board[move.row_to][move.col_to].append(piece)
+        else:
+            print("aldjasljdlaskdjlkasjsdlkjalkjd")
+            print(move.row_from, move.col_from)
+            piece = board[move.row_from][move.col_from][-1]
+            self.yank_move(board, piece, move.row_to, move.col_to,False)
+        if piece :
+            point1 = [piece.x, piece.y]
+            point2 = convert_from_row_col_to_abs_pos(move.row_to, move.col_to, 
+                            piece.button.get_width(), piece.button.get_height())
+            points = self.interpolate_path(point1, point2, 400)
+            self.move_automatically(points, piece)
+            piece.set_pos_by_row_and_col(move.row_to, move.col_to)
+            move_sound.play()
+            self.game.switch_turns()
 
-    def yank_move(self, board, pieces,row , col):
-        current_row, current_col = pieces[self.active].row , pieces[self.active].col
-        # dont count move if it is the same place 
-        if current_col == col and current_row == row :
-            return 
-        if current_col != None and current_row != None :
+    def yank_move(self, board, piece, row, col, switch_turns=True):
+        current_row, current_col = piece.row, piece.col
+        # dont count move if it is the same place
+        if current_col == col and current_row == row:
+            return None
+        from_outside = "Y"
+        aside_stack = piece.aside_stack
+        if current_col != None and current_row != None:
+            from_outside = "N"
             stack = board[current_row][current_col]
             stack.pop()
             if stack:
                 stack[-1].button.show()
 
-        board[row][col].append(pieces[self.active])
-        pieces[self.active].set_pos_by_row_and_col(row, col)
-        stack =  board[row][col]
-        if len(stack) > 1 : 
-            for i in range(len(stack)-1):
+        board[row][col].append(piece)
+        piece.set_pos_by_row_and_col(row, col)
+        stack = board[row][col]
+        if len(stack) > 1:
+            for i in range(len(stack) - 1):
                 stack[i].button.hide()
-        self.game.switch_turns()
-        move_sound.play() 
+
+        if switch_turns:
+            self.game.switch_turns()
+            move_sound.play()
+        return [from_outside, aside_stack, current_row, current_col, row, col]
 
     def return_piece(self, pieces, event):
         points = self.interpolate_path(event.pos, self.past_pos)
         self.move_automatically(points, pieces[self.active])
 
-    def check_rules(self, board, pieces,row , col,event):
-        if board[row][col]: 
-            # check gobbling rule 
+    def check_rules(self, board, pieces, row, col, event):
+        if board[row][col]:
+            # check gobbling rule
             if SIZE[board[row][col][-1].size][-1] < SIZE[pieces[self.active].size][-1]:
-                self.yank_move(board, pieces,row , col)
-            else:
-                # invalid move, return piece as it was  
-                self.return_piece(pieces, event)
-        else:
-            # the cell is empty 
-            self.yank_move(board, pieces,row , col)
+                self.augmented_move = self.yank_move(
+                    board, pieces[self.active], row, col
+                )
 
+            else:
+                # invalid move, return piece as it was
+                self.return_piece(pieces, event)
+                self.augmented_move = None
+        else:
+            # the cell is empty
+            self.augmented_move = self.yank_move(board, pieces[self.active], row, col)
--- gui/game.py
+++ gui/game.py
@@ -1,50 +1,53 @@
-import time 
-import pygame 
+import time
+import pygame
 import pygame_menu as pgm
 
 from .board import Board
 from .drag_handler import Drag_Handler
-from .constants import * 
-
-clock = pygame.time.Clock()
+from .constants import *
+from .ai_agent import Logic
 
 
 class Game:
-
     def __init__(self, game_window):
         self.game_window = game_window
-        self.board = Board(game_window)
-        self.drag_handler = Drag_Handler(self)
 
-        self.current_player = "w" # white starts by default 
-        self.game_mode = None 
-        self.difficulty = None 
-        self.intialize_labels()
-        self.turn_label = self.game_window.get_widget('turn')
+        self.game_mode = MODES[0]  # human vs human
+        self.difficulty = "easy"
+        self.ai_should_play = True
+        self.reset()
 
-        self.winner = None 
-        self.end_of_game = False
 
     def reset(self):
         self.game_window.full_reset()
         self.game_window.clear()
         self.board = Board(self.game_window)
-        self.current_player = 'w'
+        self.drag_handler = Drag_Handler(self)
+        self.current_player = "w"  # white starts by default
         self.intialize_labels()
+        self.turn_label = self.game_window.get_widget("turn")
+        self.agent = Logic()
+        self.agent.current = self.current_player
+        self.winner = None
         self.end_of_game = False
 
-    def set_difficulty(self,selected, value):
-        print(selected ," ", value)
+    def set_difficulty(self, selected, value):
+        if self.game_mode == ["human", "ai"]:
+            self.difficulty = DIFFICULTIES[value][0]  # consider the first one only
+        elif self.game_mode == ["ai", "ai"]:
+            self.difficulty = DIFFICULTIES[value]
+        print("Difficulty: ", self.difficulty)
         self.reset()
 
-    def set_game_mode(self,selected, value):
-        print(selected ," ", value)
+    def set_game_mode(self, selected, value):
+        self.game_mode = MODES[value]
+        print("Game Mode:", self.game_mode)
         self.reset()
 
     def switch_turns(self):
         self.current_player = "b" if self.current_player == "w" else "w"
         self.turn_label.set_title(PLAYER[self.current_player][0])
-        self.turn_label.update_font({"color" : PLAYER[self.current_player][2]})
+        self.turn_label.update_font({"color": PLAYER[self.current_player][2]})
         self.check_game_state()
 
     def check_game_state(self):
@@ -59,81 +62,120 @@ class Game:
     def raise_end_of_game_flag(self):
         self.end_of_game = True
 
-        # remove whose turn 
+        # remove whose turn
         self.turn_label.set_title("-")
-        self.turn_label.update_font({"color" : "#ff0000"})
-        
+        self.turn_label.update_font({"color": "#ff0000"})
+
         # declare winner
         winner = PLAYER[self.winner][0]
-        label = self.game_window.add.label(f"{winner} WON!",font_size=80, font_color="#ff0000")
+        label = self.game_window.add.label(
+            f"{winner} WON!", font_size=80, font_color="#ff0000"
+        )
         label.set_float(origin_position=True)
-        label.translate(WIDTH//2-label.get_width()//2, HEIGHT-label.get_height() - 60)
-        
+        label.translate(
+            WIDTH // 2 - label.get_width() // 2, HEIGHT - label.get_height() - 60
+        )
+
         # self.reset()
 
     def check_draw_state(self):
         pass
-    def move_piece_by_human(self):
+
+    def play(self, events):
+        # if self.game_mode == ["human", "human"] :
+            # self.human_vs_human_mode(events)
+        # elif self.game_mode == ["human", "ai"]:
+        self.human_vs_ai_mode(events)
+        # else:
+        #     self.ai_vs_ai_mode()
+
+    def human_vs_human_mode(self, events):
+        self.drag_handler.update(events, is_ai_player=False)
+        aug_move = self.drag_handler.augmented_move
+        if aug_move:
+            self.agent.play_by_human(self.agent.current, aug_move[:4], aug_move[4:])
+            self.drag_handler.augmented_move = None
+            self.agent.set_winner()
+            if self.agent.winner:
+                print("Winner from logic: ", self.agent.winner)
+            self.agent.switch_turns()
+
+    def human_vs_ai_mode(self, events):
+        is_ai_player = (self.current_player == "w")
+        if is_ai_player and self.ai_should_play:
+            ai_move = self.agent.play_by_ai("easy")
+            self.drag_handler.update(events, is_ai_player, ai_move)
+            self.ai_should_play = False
+            self.agent.switch_turns()
+            self.board.print_board()
+        elif not is_ai_player and not self.ai_should_play:
+            self.drag_handler.update(events)
+            aug_move = self.drag_handler.augmented_move
+            if aug_move is not None:
+                self.agent.play_by_ai("easy", aug_move[:4], aug_move[4:])
+                self.drag_handler.augmented_move = None
+                self.ai_should_play = True
+                self.agent.switch_turns()
+        self.agent.set_winner()
+        if self.agent.winner:
+            pass
+            # print("winner from logic :", self.agent.winner)
+            # quit()
+    def ai_vs_ai_mode(self):
         pass
-
-    def move_piece_by_AI(self):
-        pass
-
-    def check_collision(self,events):
-        self.drag_handler.update(events)
-
     def get_board(self):
         return self.board
 
     def get_current_player(self):
-        return self.current_player 
+        return self.current_player
 
     def intialize_labels(self):
-        label = self.game_window.add.label("Turn",font_size=50, font_color="#f0ff00")
+        label = self.game_window.add.label("Turn", font_size=50, font_color="#f0ff00")
         label.set_float(origin_position=True)
-        label.translate(WIDTH//2, 30)
+        label.translate(WIDTH // 2, 30)
 
-        turn_label = self.game_window.add.label("White", label_id = "turn",font_size=50, font_color="#ffffff")
+        turn_label = self.game_window.add.label(
+            "White", label_id="turn", font_size=50, font_color="#ffffff"
+        )
         turn_label.set_float(origin_position=True)
-        turn_label.translate(WIDTH//2 - 130 , 30)
-
+        turn_label.translate(WIDTH // 2 - 130, 30)
 
     def check_row_win(self):
         for row in range(BOARD_ROWS):
             top_colors_row = []
             for column in range(BOARD_COLS):
-                top =  self.board.get_top_piece(row, column)
-                if not top :
+                top = self.board.get_top_piece(row, column)
+                if not top:
                     break
                 top_colors_row.append(top.player)
 
-            if not top_colors_row: 
+            if not top_colors_row:
                 continue
             if self.count_colors(top_colors_row, "Row Win: "):
                 return
 
     def check_col_win(self):
-        for column  in range(BOARD_COLS):
+        for column in range(BOARD_COLS):
             top_colors_col = []
             for row in range(BOARD_ROWS):
-                top =  self.board.get_top_piece(row, column)
-                if not top :
+                top = self.board.get_top_piece(row, column)
+                if not top:
                     break
                 top_colors_col.append(top.player)
 
-            if not top_colors_col: 
+            if not top_colors_col:
                 continue
 
-            if self.count_colors(top_colors_col, "Col Win: "): 
+            if self.count_colors(top_colors_col, "Col Win: "):
                 return
 
     def check_diagonal_win(self, right=False):
-        # default is left diagonal checking 
+        # default is left diagonal checking
         top_colors_diagonal = []
         for i in range(BOARD_COLS):
-            y = 3 - i if right else i 
-            top =  self.board.get_top_piece(i, y)
-            if not top :
+            y = 3 - i if right else i
+            top = self.board.get_top_piece(i, y)
+            if not top:
                 break
             top_colors_diagonal.append(top.player)
 
@@ -145,7 +187,7 @@ class Game:
         # print(colors)
         if colors.count(colors[0]) == 4:
             self.winner = colors[0]
-            if msg :
+            if msg:
                 print(msg + self.winner)
-            return True  # there is a winner 
-        return False     # game continues 
+            return True  # there is a winner
+        return False  # game continues
