90c5f7da5f2b82368bb54d05055446aece72f9a8
Mohamed Rezk <mohrizq895@gmail.com>
2024-01-29 01:25:18 +0200

    Inhibit pygame welcome message

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
 import pygame
 import pygame_menu as pgm
-import os
-import sys
 
 from src.gui.board import Board 
 from src.algorithm.state import State 
 from src.algorithm.ai_agent import AI_Agent
 from src.algorithm.check_winner import check_winner
+from src.algorithm.heuristics import evaluate_state
 
 
 cli =  0
@@ -50,15 +52,20 @@ else :
     w_agent  = AI_Agent("w", "hard")
     b_agent  = AI_Agent("b", "easy")
     player = "w"
+    i = 0
     winner = False
     while not winner:
+        print("before applying: ", evaluate_state(state, player))
         if player == "w": 
             ai_move = w_agent.play_by_ai(state)
         else:
             ai_move = b_agent.play_by_ai(state)
         print(player, ai_move, ai_move.piece.aside_stack)
+        state.apply_move(ai_move,ai_move.piece.player)
+        # print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+        # print("after applying: ", evaluate_state(state, player))
+        # print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
         print("-"*30)
-        state = state.apply_move(ai_move,ai_move.piece.player)
         winner = check_winner(state)
         if winner :
             break
@@ -67,6 +74,9 @@ else :
         print(state.stacks[player])
         player = "w" if player == "b" else "b"
         print("*"*30)
+        # i += 1
+        # if i == 2:
+        #     break
     print(check_winner(state))
 
 
