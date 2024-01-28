f9ec86430430c153ee7969d74d9f9e99162b8506
Mohamed Rezk <mohrizq895@gmail.com>
2024-01-29 01:19:28 +0200

    Implement undo move to replace deepcopy

--- src/algorithm/state.py
+++ src/algorithm/state.py
@@ -8,6 +8,22 @@ from ..gui.board import Board
 from .check_winner import check_winner
 from .check_winner import count_pieces_in_line
 
+MY_GOBBLING_WEIGHT = 1
+OTHER_GOBBLING_WEIGHT = 3
+EMPTY_CELL_WEIGHT = 2
+
+def is_piece_in_the_same_line(piece,row,col,check):
+
+    if piece.is_outside():
+        return True
+    if check == "row" :
+        return piece.row == row
+    elif check == "col" :
+        return piece.col == col
+    elif check == "left":
+        return piece.col == piece.row
+    else:
+        return piece.col == (3 - piece.row) 
 
 @dataclass
 class Piece_Move:
@@ -15,6 +31,9 @@ class Piece_Move:
     row_to: int
     col_to: int
     from_outside: bool
+    row_from: int
+    col_from: int
+    weight: int
 
 
 class State:
@@ -44,38 +63,48 @@ class State:
                 movable_pieces.append(stack[-1])
         return movable_pieces
 
-    def get_valid_piece_moves(self, piece, from_outside, include_winning_moves= False):
-        if include_winning_moves :
+    def get_valid_piece_moves(self, piece, from_outside, include_winning_moves=False):
+        if include_winning_moves:
             winning_moves = self.has_a_win_move(piece)
             if winning_moves:
-                # print(winning_moves)
+                # print("winning:", winning_moves)
                 return winning_moves, True
         valid_piece_moves = []
         for row in range(4):
             for col in range(4):
-                move = Piece_Move(piece, row, col, from_outside)
+                move = Piece_Move(piece, row, col, from_outside, piece.row, piece.col,0)
                 if self.is_valid_piece_move(move):
                     valid_piece_moves.append(move)
         return valid_piece_moves, False
 
     def has_a_win_move(self, target_piece):
         winning_moves = []
-
-        def helper(count, x, y, open_place):
+        def helper(count, x, y, open_place, check):
             nonlocal winning_moves
-            # open_place = None
             piece = self.get_top_piece(x, y)
             if not piece:
-                open_place = (x, y)
+                # exclude pieces in that line
+                if not is_piece_in_the_same_line(target_piece,x,y, check):
+                    open_place = (x, y)
             elif piece.player != target_piece.player and piece.size < target_piece.size:
                 open_place = (x, y)
             elif piece.player == target_piece.player:
                 count += 1
-            # if count == 3 and open_place:
-            #     is_outside = target_piece.row == None and target_piece.col == None
-            #     winning_moves.append(Piece_Move(target_piece, open_place[0], open_place[1], is_outside))
-            #     count = 0 
-            #     open_place = None
+            if count == 3 and open_place:
+                is_outside = target_piece.is_outside()
+                winning_moves.append(
+                    Piece_Move(
+                        target_piece,
+                        open_place[0],
+                        open_place[1],
+                        is_outside,
+                        target_piece.row,
+                        target_piece.col,
+                        100
+                    )
+                )
+                count = 0
+                open_place = None
             return count, open_place
 
         l_diag_count, open_l_diag = 0, None
@@ -84,36 +113,16 @@ class State:
             row_count, open_row = 0, None
             col_count, open_col = 0, None
             for col in range(4):
-                # print(col, row)
-                row_count, open_row = helper(row_count, row, col, open_row)
-                if row_count == 3 and open_row:
-                    is_outside = target_piece.row == None and target_piece.col == None
-                    winning_moves.append(Piece_Move(target_piece, open_row[0], open_row[1], is_outside))
-                    row_count = 0 
-                    open_row = None
-
-                col_count, open_col = helper(col_count, col, row, open_col)
-                if col_count == 3 and open_col:
-                    is_outside = target_piece.row == None and target_piece.col == None
-                    winning_moves.append(Piece_Move(target_piece, open_col[0], open_col[1], is_outside))
-                    col_count = 0 
-                    open_col = None
-
-            l_diag_count, open_l_diag = helper(l_diag_count, row, row, open_l_diag)
-            if l_diag_count == 3 and open_l_diag:
-                is_outside = target_piece.row == None and target_piece.col == None
-                winning_moves.append(Piece_Move(target_piece, open_l_diag[0], open_l_diag[1], is_outside))
-                l_diag_count = 0 
-                open_l_diag = None
-
-            r_diag_count, open_r_diag = helper(r_diag_count, row, 3 - row, open_r_diag)
-            if r_diag_count == 3 and open_r_diag:
-                is_outside = target_piece.row == None and target_piece.col == None
-                winning_moves.append(Piece_Move(target_piece, open_r_diag[0], open_r_diag[1], is_outside))
-                r_diag_count = 0 
-                open_r_diag = None
-
-        # print("*"*30)
+                row_count, open_row = helper(row_count, row, col, open_row, "row")
+                col_count, open_col = helper(col_count, col, row, open_col, "col")
+            if winning_moves:
+                return winning_moves
+            l_diag_count, open_l_diag = helper(l_diag_count, row, row, open_l_diag, "left")
+            if winning_moves:
+                return winning_moves
+            r_diag_count, open_r_diag = helper(r_diag_count, row, 3 - row, open_r_diag, "right")
+            if winning_moves:
+                return winning_moves
         return winning_moves
 
     def get_valid_moves(self, player, include_winning_moves):
@@ -121,8 +130,9 @@ class State:
         movable_pieces = self.get_movable_pieces(player)
         has_winning = False
         for piece in movable_pieces:
-            is_outside = piece.row == None and piece.col == None
-            moves, has_winning = self.get_valid_piece_moves(piece, is_outside,include_winning_moves)
+            moves, has_winning = self.get_valid_piece_moves(
+                piece, piece.is_outside(), include_winning_moves
+            )
             valid_moves.extend(moves)
         shuffle(valid_moves)
         return valid_moves, has_winning
@@ -130,35 +140,43 @@ class State:
     def is_valid_piece_move(self, move):
         top_piece = self.get_top_piece(move.row_to, move.col_to)
         if top_piece is None:  # cell is empty
+            move.weight = EMPTY_CELL_WEIGHT
             return True
-        elif top_piece.size >= move.piece.size:
-            return False
-        return True
+        elif top_piece.size < move.piece.size:  # can gobble
+            if top_piece.player == move.piece.player:
+                move.weight = MY_GOBBLING_WEIGHT
+            else:
+                move.weight = OTHER_GOBBLING_WEIGHT
+            return True
+        return False
 
     def get_top_piece(self, row, col):
         if self.board[row][col]:
             return self.board[row][col][-1]
         return None
 
-    def apply_move(self, move, player):
+    def undo_move(self, move, player):
+        if self.board[move.row_to][move.col_to][-1].piece_id != move.piece.piece_id:
+            raise Exception("Not the same piece")
 
-        if player != move.piece.player :
-            raise Exception("different players")
-        new_stacks = deepcopy(self.stacks)
-        new_board = deepcopy(self.board)
-        # new_board = jsonpickle.decode(jsonpickle.encode(self.board))
-        # new_stacks = jsonpickle.decode(jsonpickle.encode(self.stacks))
+        if player != move.piece.player:
+            raise Exception("Different players")
+
+        self.board[move.row_to][move.col_to].pop()
+        move.piece.row = move.row_from
+        move.piece.col = move.col_from
         if move.from_outside:
-            new_stacks[player][move.piece.aside_stack].pop()
+            self.stacks[player][move.piece.aside_stack].append(move.piece)
         else:
-            new_board[move.piece.row][move.piece.col].pop()
-        new_piece = Abstract_Piece(
-            move.piece.piece_id,
-            move.piece.player,
-            move.piece.size,
-            move.piece.aside_stack,
-            move.row_to,
-            move.col_to,
-        )
-        new_board[move.row_to][move.col_to].append(new_piece)
-        return State(new_board, new_stacks)
+            self.board[move.row_from][move.col_from].append(move.piece)
+
+    def apply_move(self, move, player):
+        if player != move.piece.player:
+            raise Exception("Different players")
+        if move.from_outside:
+            self.stacks[player][move.piece.aside_stack].pop()
+        else:
+            self.board[move.piece.row][move.piece.col].pop()
+        move.piece.row = move.row_to
+        move.piece.col = move.col_to
+        self.board[move.row_to][move.col_to].append(move.piece)
